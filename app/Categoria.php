af859fa1d2729704675dfafa4d849c89e6e0842d
nestor <nmoreno@overcom.net>
2018-04-05 11:23:52 -0400

    Se agrego modelo, migracion, controlador, componente de vista y metodos Vue.js de categorias

---
+++ app/Categoria.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Categoria extends Model
+{
+    /*
+    protected $table = 'categorias';
+    protected $primaryKey = 'id';
+    */
+    protected $fillable = ['nombre', 'descripcion', 'condicion'];
+}
