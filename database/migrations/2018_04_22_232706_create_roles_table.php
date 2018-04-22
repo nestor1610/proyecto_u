491dc904e817362677d970819ce797b87c9adb8e
nestor <nmoreno@overcom.net>
2018-04-22 19:34:19 -0400

    se creo migracion roles

---
+++ database/migrations/2018_04_22_232706_create_roles_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRolesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('roles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nombre', 30)->nullable();
+            $table->string('descripcion', 100)->nullable();
+            $table->boolean('condicion')->default(1);
+        });
+
+        DB::table('roles')->insert([
+            'id' => 1,
+            'nombre' => 'Administrador',
+            'descripcion' => 'Administradores de area'
+        ]);
+
+        DB::table('roles')->insert([
+            'id' => 2,
+            'nombre' => 'Vendedor',
+            'descripcion' => 'Vendedor area venta'
+        ]);
+
+        DB::table('roles')->insert([
+            'id' => 3,
+            'nombre' => 'Almacenero',
+            'descripcion' => 'Almacenero area compras'
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('roles');
+    }
+}
